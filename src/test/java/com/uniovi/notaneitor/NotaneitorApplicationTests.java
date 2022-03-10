c8e4bb56662c0030aa5f34588563a5ab9ab6ad51
Sara Fernández Arias <uo269546@uniovi.es>
2022-03-10 15:38:52 +0100

    SDI-2122-307-5.5-Casos Uso VistaSignUp.

--- src/test/java/com/uniovi/notaneitor/NotaneitorApplicationTests.java
+++ src/test/java/com/uniovi/notaneitor/NotaneitorApplicationTests.java
@@ -2,6 +2,8 @@ package com.uniovi.notaneitor;
 
 import com.uniovi.notaneitor.pageobjects.PO_HomeView;
 import com.uniovi.notaneitor.pageobjects.PO_Properties;
+import com.uniovi.notaneitor.pageobjects.PO_SignUpView;
+import com.uniovi.notaneitor.pageobjects.PO_View;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,7 +19,6 @@ class NotaneitorApplicationTests {
     static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
     static String Geckodriver = "C:\\Users\\Sara\\Desktop\\Universidad\\3-tercer curso\\segundo cuatri\\(SDI)-Sistemas Distribuidos e Internet\\Sesión5-material\\geckodriver-v0.30.0-win64.exe";
     static String URL = "http://localhost:8090";    //Común a Windows y a MACOSX
-    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
 
     public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
         System.setProperty("webdriver.firefox.bin", PathFirefox);
@@ -28,7 +29,7 @@ class NotaneitorApplicationTests {
 
     @BeforeAll
     static public void begin() {
-    }
+    }    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
 
     //Al finalizar la última prueba
     @AfterAll
@@ -46,8 +47,6 @@ class NotaneitorApplicationTests {
         driver.navigate().to(URL);
     } //Después de cada prueba se borran las cookies del navegador
 
-    //Antes de la primera prueba
-
     @AfterEach
     public void tearDown() {
         driver.manage().deleteAllCookies();
@@ -60,6 +59,8 @@ class NotaneitorApplicationTests {
         PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
     }
 
+    //Antes de la primera prueba
+
     @Test
     @Order(2)
     void PR01B() {
@@ -69,11 +70,105 @@ class NotaneitorApplicationTests {
     }
 
     //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
-    @Test @Order(3) public void PR02() { PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary"); }
-    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
-    @Test @Order(4) public void PR03() { PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary"); }
-    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
-    @Test @Order(5) public void PR04() { PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH()); }
+    @Test
+    @Order(3)
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
     }
+
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+    @Test
+    @Order(4)
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+    @Test
+    @Order(5)
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
+
+    //PR05->En este caso vamos al formulario de registro, lo rellenamos y esperamos a visualizar la cabecera de la lista de notas para ese alumno (“Notas del usuario”)
+    @Test
+    @Order(6)
+    public void PR05() {
+        //Vamos al formulario de registro
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        //Rellenamos el formulario.
+        PO_SignUpView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+        //Comprobamos que entramos en la sección privada y nos nuestra el texto a buscar
+        String checkText = "Notas del usuario";
+        List<WebElement> result = PO_View.checkElementBy(driver, "text", checkText);
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+
+    //PR06A. Prueba del formulario de registro. DNI repetido en la BD
+    // Propiedad: Error.signup.dni.duplicate
+    @Test
+    @Order(7)
+    public void PR06A() {
+
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777", "77777");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+
+        //Comprobamos el error de DNI repetido.
+        String checkText = PO_HomeView.getP().getString("Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+
+    //PR06B. Prueba del formulario de registro. Nombre corto. // Propiedad: Error.signup.dni.length
+    @Test
+    @Order(8)
+    public void PR06B() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990B", "Jose", "Perez", "77777", "77777");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.name.length", PO_Properties.getSPANISH());
+        //Comprobamos el error de Nombre corto de nombre corto .
+        String checkText = PO_HomeView.getP().getString("Error.signup.name.length", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+
+    //PR06C. Prueba del formulario de registro. Apellido corto.
+    // Propiedad: Error.signup.lastName.length
+    @Test
+    @Order(9)
+    public void PR06C() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990B", "Josefa", "Pe", "77777", "77777");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.lastName.length", PO_Properties.getSPANISH());
+        //Comprobamos el error
+        String checkText = PO_HomeView.getP().getString("Error.signup.lastName.length", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+
+    //PR06D. Prueba del formulario de registro. Contraseña corta.
+    // Propiedad: Error.signup.password.length
+    @Test
+    @Order(10)
+    public void PR06D() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990B", "Josefa", "Benítez", "7", "7");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.password.length", PO_Properties.getSPANISH());
+        //Comprobamos el error
+        String checkText = PO_HomeView.getP().getString("Error.signup.password.length", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+    //PR06E: Prueba del formulario de registro. Contraseñas no coinciden..
+    // Propiedad: Error.signup.passwordConfirm.coincidence
+    @Test
+    @Order(11)
+    public void PR06E() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+        PO_SignUpView.fillForm(driver, "99999990B", "Josefa", "Benítez", "77141234", "123444221");
+        List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.passwordConfirm.coincidence", PO_Properties.getSPANISH());
+        //Comprobamos el error
+        String checkText = PO_HomeView.getP().getString("Error.signup.passwordConfirm.coincidence", PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+    }
+    //Error.signup.passwordConfirm.coincidence
+}
 
 
